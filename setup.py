4008d0e69327562536ee45175c7aa576cea8a7f8
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-01 01:12:20 +0100

    feat(setup): more metadata

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
+def readme():
+  with open("README.md") as f:
+    return f.read()
+
 setup(
   name="yamlcredits",
   version="1.0.0",
   description="Small tool for generating a CREDITS.md from YAML files.",
+  long_description=readme(),
+  url="http://github.com/tribecamp/yamlcredits",
   author="Tribecamp",
   author_email="support@tribecamp.com",
+  license="MIT",
+  keywords="tribecamp yaml credits markdown",
+  python_requires=">=3.6",
   packages=["yamlcredits"],
   scripts=["bin/yamlcredits"],
   install_requires=[
